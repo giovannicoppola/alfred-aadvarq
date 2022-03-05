c06c1a262583bcd8529f63ec1f95390aae7a951c
Giovanni <5774411+giovannicoppola@users.noreply.github.com>
2022-03-05 09:39:53 -0500

    first commit

---
+++ source/aadv.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3 
+# giovanni
+# Saturday, March 5, 2022, 8:46 AM
+# Sunny ☀️   🌡️+30°F (feels +29°F, 51%) 🌬️↙4mph 🌒
+
+import sys
+import json
+import os
+
+MAXLENGTH = os.path.expanduser(os.getenv('MAXLENGTH', '0'))
+
+def log(s, *args):
+    if args:
+        s = s % args
+    print(s, file=sys.stderr)
+
+myLog = "".join([i for i in sys.stdin])
+
+myTotal=len(myLog.split('\n'))-1
+
+
+result = {"items": []}
+
+#log (myOut)
+
+def main ():
+    myCount = 0
+    for T in myLog.splitlines():
+        fullT=T
+        lenT = len (T)
+        if lenT > int(MAXLENGTH):
+            T = T[1:50]+" ... "+T[80:len(T)]
+        myCount += 1
+        fileName= os.path.basename(T)
+        result["items"].append({
+                    "title": fileName,
+                    "subtitle": str(myCount) + "/"+ str(myTotal) + "-"+T,
+                    
+                    "valid":'TRUE',
+                            
+                    "arg":fullT})
+                
+
+    print (json.dumps(result))
+
+if __name__ == "__main__":
+    main()
---
+++ source/stringCreation.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3 
+
+## STRING CREATION
+# giovanni
+# Saturday, March 5, 2022, 8:46 AM
+# Sunny ☀️   🌡️+30°F (feels +29°F, 51%) 🌬️↙4mph 🌒
+
+
+import sys
+
+def log(s, *args):
+    if args:
+        s = s % args
+    print(s, file=sys.stderr)
+
+
+myString =sys.argv[1]
+
+myList=myString.split()
+
+#log (len(myList))
+
+myFirst = myList[0]
+myOutput = 'tag:'+myFirst
+
+if len(myList)>1:
+ 
+    
+    for ele in myList[1:]:
+        if ele in ['AND','OR','NOT']:
+            myOutput = myOutput+" "+ele        
+        else:
+            myOutput = myOutput+" tag:"+ele        
+        
+    
+    
+    
+
+
+
+#log (myOutput)
+
+
+print (myOutput)
